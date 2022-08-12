6f760a5019777138157df8166dafadbf972af91c
Jayden Sipe <jayden.sipe@gmail.com>
2022-08-12 19:50:07 -0400

    Website style changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,121 +4,218 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body'),
 		$main = $('#main');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px']
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-		
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
 
 	// Nav.
-		var $nav = $('#nav');
+	var $nav = $('#nav');
 
-		if ($nav.length > 0) {
+	if ($nav.length > 0) {
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+		// Shrink effect.
+		$main
+			.scrollex({
+				mode: 'top',
+				enter: function () {
+					$nav.addClass('alt');
+				},
+				leave: function () {
+					$nav.removeClass('alt');
+				},
+			});
 
-			// Links.
-				var $nav_a = $nav.find('a');
+		// Links.
+		var $nav_a = $nav.find('a');
 
+		$nav_a
+			.scrolly({
+				speed: 1000,
+				offset: function () { return $nav.height(); }
+			})
+			.on('click', function () {
+
+				var $this = $(this);
+
+				// External link? Bail.
+				if ($this.attr('href').charAt(0) != '#')
+					return;
+
+				// Deactivate all links.
 				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+					.removeClass('active')
+					.removeClass('active-locked');
 
-						var $this = $(this);
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+				$this
+					.addClass('active')
+					.addClass('active-locked');
 
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
+			})
+			.each(function () {
 
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
+				var $this = $(this),
+					id = $this.attr('href'),
+					$section = $(id);
 
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
+				// No section for this link? Bail.
+				if ($section.length < 1)
+					return;
 
-					})
-					.each(function() {
+				// Scrollex.
+				$section.scrollex({
+					mode: 'middle',
+					initialize: function () {
 
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
+						// Deactivate section.
+						if (browser.canUse('transition'))
+							$section.addClass('inactive');
 
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
+					},
+					enter: function () {
 
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
+						// Activate section.
+						$section.removeClass('inactive');
 
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
+						// No locked links? Deactivate all links and activate this section's one.
+						if ($nav_a.filter('.active-locked').length == 0) {
 
-								},
-								enter: function() {
+							$nav_a.removeClass('active');
+							$this.addClass('active');
 
-									// Activate section.
-										$section.removeClass('inactive');
+						}
 
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
+						// Otherwise, if this section's link is the one that's locked, unlock it.
+						else if ($this.hasClass('active-locked'))
+							$this.removeClass('active-locked');
 
-											$nav_a.removeClass('active');
-											$this.addClass('active');
+					}
+				});
 
-										}
+			});
 
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
-
-								}
-							});
-
-					});
-
-		}
+	}
 
 	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
-		});
+	$('.scrolly').scrolly({
+		speed: 1000
+	});
 
 })(jQuery);
+
+// Controls styling of website based on month\
+var date = new Date();
+var month = new Date().getMonth() + 1;
+
+switch (month) {
+	case 10:
+		$('#main').addClass('halloween-background');
+		break;
+	case 8:
+		if (date.getUTCDate() === 12) {
+			$('#main').addClass('new-years-background');
+
+			tsParticles.load({ preset: "fireworks" });
+
+			break;
+		}
+
+		$('#main').addClass('christmas-background');
+
+		// Controls particles, CREDIT: https://codepen.io/dariodev/pen/JRqbgK
+		particlesJS("particles-js", {
+			particles: {
+				number: {
+					value: 52,
+					density: {
+						enable: true,
+						value_area: 631.3280775270874
+					}
+				},
+				color: {
+					value: "#fff"
+				},
+				shape: {
+					type: "circle",
+					stroke: {
+						width: 0,
+						color: "#000000"
+					},
+					polygon: {
+						nb_sides: 5
+					},
+					image: {
+						src: "img/github.svg",
+						width: 100,
+						height: 100
+					}
+				},
+				opacity: {
+					value: 0.5,
+					random: true,
+					anim: {
+						enable: false,
+						speed: 1,
+						opacity_min: 0.1,
+						sync: false
+					}
+				},
+				size: {
+					value: 5,
+					random: true,
+					anim: {
+						enable: false,
+						speed: 40,
+						size_min: 0.1,
+						sync: false
+					}
+				},
+				line_linked: {
+					enable: false,
+					distance: 500,
+					color: "#ffffff",
+					opacity: 0.4,
+					width: 2
+				},
+				move: {
+					enable: true,
+					speed: 1.5,
+					direction: "bottom",
+					random: false,
+					straight: false,
+					out_mode: "out",
+					bounce: false,
+					attract: {
+						enable: false,
+						rotateX: 600,
+						rotateY: 1200
+					}
+				}
+			},
+			retina_detect: true
+		});
+		break;
+	default:
+		$('#main').addClass('regular-background');
+		break;
+}

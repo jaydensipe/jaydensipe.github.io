462c05c082ca35bf148c146c3bff248fe9a8fc73
Jayden Sipe <jayden.sipe@gmail.com>
2024-02-13 00:49:26 -0500

    Add workflows to build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,1 @@
-// Disables SSR to turn off the server-side rendering for site, creating a single-page application (SPA) instead of a server-rendered application.
-export const ssr = false;
-
+export const prerender = true;

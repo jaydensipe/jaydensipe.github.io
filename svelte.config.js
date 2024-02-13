960b0907bc898f5f01dde78475cf6ab532860b74
Jayden Sipe <jayden.sipe@gmail.com>
2024-02-13 01:15:11 -0500

    Updated config

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.argv.includes('dev') ? '' : "/jaydensipe.github.io"
+      base: process.env.NODE_ENV === "production" ? "/jaydensipe.github.io" : "",
     }
   },
 };

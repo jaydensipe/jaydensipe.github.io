fa0648cbd1f8caa648eb360981c1bd4888480f70
Jayden Sipe <jayden.sipe@gmail.com>
2025-07-07 02:09:48 -0400

    Add Motion One support

--- src/lib/components/jay/descriptive-card/descriptive-card.svelte
+++ src/lib/components/jay/descriptive-card/descriptive-card.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import * as Card from "$lib/components/ui/card";
     import Separator from "$lib/components/ui/separator/separator.svelte";
-    import Saos from "saos";
     interface Props {
         name: string;
         link?: string;
@@ -24,58 +23,53 @@
 </script>
 
 <div>
-    <Saos
-        once
-        animation={"scale-in-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
+    <Card.Root
+        class="bg-transparent w-[21rem] text-center border-0 shadow-none"
     >
-        <Card.Root
-            class="bg-transparent w-[21rem] text-center border-0 shadow-none"
-        >
-            <Card.Header>
-                {#if link}
-                    <a class="m-0 p-0" href={link}>
-                        <enhanced:img
-                            loading="lazy"
-                            class="{noFillBgColor} w-full scale-90 sm:scale-100 shadow-lg rounded-lg motion-reduce:hover:scale-100 sm:hover:scale-110 hover:motion-reduce:skew-y-2 transition duration-150 ease-out"
-                            src={imgSrc}
-                            alt={altImageText}
-                        /></a
-                    >
-                {:else}
+        <Card.Header>
+            {#if link}
+                <a class="m-0 p-0" href={link}>
                     <enhanced:img
                         loading="lazy"
                         class="{noFillBgColor} w-full scale-90 sm:scale-100 shadow-lg rounded-lg motion-reduce:hover:scale-100 sm:hover:scale-110 hover:motion-reduce:skew-y-2 transition duration-150 ease-out"
                         src={imgSrc}
                         alt={altImageText}
-                    />
+                    /></a
+                >
+            {:else}
+                <enhanced:img
+                    loading="lazy"
+                    class="{noFillBgColor} w-full scale-90 sm:scale-100 shadow-lg rounded-lg motion-reduce:hover:scale-100 sm:hover:scale-110 hover:motion-reduce:skew-y-2 transition duration-150 ease-out"
+                    src={imgSrc}
+                    alt={altImageText}
+                />
+            {/if}
+            <Card.Title class="pt-6">
+                {#if link}
+                    <a
+                        class="hyperlink card-title text-lg leading-none"
+                        href={link}>{@html name}</a
+                    >
+                {:else}
+                    <span class="card-title text-lg leading-none">
+                        {@html name}
+                    </span>
                 {/if}
-                <Card.Title class="pt-6">
-                    {#if link}
-                        <a
-                            class="hyperlink card-title text-lg leading-none"
-                            href={link}>{@html name}</a
-                        >
-                    {:else}
-                        <span class="card-title text-lg leading-none">
-                            {@html name}
-                        </span>
-                    {/if}
 
-                    {#if githubLink}
-                        |
-                        <a
-                            class="hyperlink tracking-wide font-bold text-lg leading-none"
-                            href={githubLink}>GitHub</a
-                        >
-                    {/if}
-                </Card.Title>
-            </Card.Header>
-            <Card.Content>
-                <p class="text-sm md:text-base">
-                    {description}
-                </p>
-            </Card.Content>
-        </Card.Root>
-        <Separator class="sm:hidden"></Separator>
-    </Saos>
+                {#if githubLink}
+                    |
+                    <a
+                        class="hyperlink tracking-wide font-bold text-lg leading-none"
+                        href={githubLink}>GitHub</a
+                    >
+                {/if}
+            </Card.Title>
+        </Card.Header>
+        <Card.Content>
+            <p class="text-sm md:text-base">
+                {description}
+            </p>
+        </Card.Content>
+    </Card.Root>
+    <Separator class="sm:hidden"></Separator>
 </div>
--- src/lib/components/jay/project-card/project-card.svelte
+++ src/lib/components/jay/project-card/project-card.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import Saos from "saos";
+    import { inView, animate } from "motion";
+    import { onMount } from "svelte";
+
     interface Props {
         name: string;
         language: string;
@@ -21,58 +23,62 @@
         description,
         isArchived = false,
     }: Props = $props();
+
+    onMount(() => {
+        inView(".project-card", (element) => {
+            animate(element, {
+                opacity: [0, 1],
+                transform: ["translateY(-20px)", "translateY(0)"],
+                duration: 300,
+                easing: "anticipate",
+            });
+        });
+    });
 </script>
 
-<Saos
-    once
-    animation={"swing-in-top-fwd 0.45s cubic-bezier(0.230, 1.000, 0.320, 1.000) both"}
->
-    <div class="pl-4 pr-4 pb-8">
-        <div
-            class="text-center flex flex-col items-center sm:border-0 p-4 text-card-foreground {!isArchived
-                ? 'bg-secondary shadow-md rounded-md'
-                : ''}"
-        >
+<div class="pl-4 pr-4 pb-8">
+    <div
+        class="text-center flex flex-col items-center sm:border-0 p-4 text-card-foreground {!isArchived
+            ? 'bg-secondary shadow-md rounded-md project-card'
+            : ''}"
+    >
+        <div class="flex flex-col sm:flex-row items-center justify-center">
             <div
-                class="flex flex-col sm:flex-row items-center justify-center"
+                class="sm:mr-auto left-4 max-w-36 max-h-36 sm:max-w-24 sm:max-h-24 p-4"
             >
-                <div
-                    class="sm:mr-auto left-4 max-w-36 max-h-36 sm:max-w-24 sm:max-h-24 p-4"
-                >
-                    <enhanced:img
-                        class="rounded-lg"
-                        src={imgSrc}
-                        alt={altImageText}
-                    />
-                </div>
-                <div class="card-title text-sm sm:text-xl max-w-lg">
-                    {#if githubLink}
-                        <a
-                            class="font-extrabold hyperlink underline-offset-4 text-cyan-700 dark:text-cyan-500"
-                            href={githubLink}
-                        >
-                            {name}</a
-                        >
-                    {:else}
-                        <span
-                            class="text-cyan-700 dark:text-cyan-500 font-extrabold"
-                            >{name}</span
-                        >
-                    {/if}
-                    created with
-                    <i class="text-cyan-700 dark:text-cyan-500">{language} </i>
-                </div>
-                <div class="flex flex-row gap-4 justify-center p-4">
-                    {#each languageIcons as icon}
-                        {@const SvelteComponent = icon}
-                        <SvelteComponent style="font-size: 1.5rem;"
-                        ></SvelteComponent>
-                    {/each}
-                </div>
+                <enhanced:img
+                    class="rounded-lg"
+                    src={imgSrc}
+                    alt={altImageText}
+                />
             </div>
-            <p class="mt-4 text-sm sm:text-base">
-                {description}
-            </p>
+            <div class="card-title text-sm sm:text-xl max-w-lg">
+                {#if githubLink}
+                    <a
+                        class="font-extrabold hyperlink underline-offset-4 text-cyan-700 dark:text-cyan-500"
+                        href={githubLink}
+                    >
+                        {name}</a
+                    >
+                {:else}
+                    <span
+                        class="text-cyan-700 dark:text-cyan-500 font-extrabold"
+                        >{name}</span
+                    >
+                {/if}
+                created with
+                <i class="text-cyan-700 dark:text-cyan-500">{language} </i>
+            </div>
+            <div class="flex flex-row gap-4 justify-center p-4">
+                {#each languageIcons as icon}
+                    {@const SvelteComponent = icon}
+                    <SvelteComponent style="font-size: 1.5rem;"
+                    ></SvelteComponent>
+                {/each}
+            </div>
         </div>
+        <p class="mt-4 text-sm sm:text-base">
+            {description}
+        </p>
     </div>
-</Saos>
+</div>
